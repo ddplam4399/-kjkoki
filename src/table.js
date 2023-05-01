8529520e91a7122a474ce03cdda9c8a94d2823e7
Plxmy <265345221018-st@rmutsb.ac.th>
2023-05-01 12:00:25 +0700

    first commit

--- src/table.js
+++ src/table.js
@@ -1,41 +1,43 @@
 import React from 'react'
+import Button from './func-event'
 
 export default function Table(){
     const data=[
-            ['JavaScript',100],
+            ['JavaScript',100,],
             ['React',200],
             ['React-Native',150]
-    
 
     ]
     const onMuseOverRow = (ev) =>{
-        ev.target.parentNode.style.backgroundColor='Blue'
+        ev.target.parentNode.style.backgroundColor='Yellow'
     }
-    const onMuseOutRow = (ev) =>{
+    const onMuseOverOut = (ev) =>{
         ev.target.parentNode.style.backgroundColor='White'
     }
-    const onClickButtonOK =() =>{
-        alert('OK')
-    }
+
+    const onClick = (ev) =>{
+        let cal = ev.target.innerText
+            alert(`${cal}`)
+     }
     return(
         <table border="1">
             <tr>
                 <th>Product</th>
                 <th>Price</th>
                 <th>Select</th>
-
             </tr>
     {       
         data.map(item =>{
             return (
             <tr onMouseOver={onMuseOverRow}
-                onMouseOut={onMuseOutRow}>
-
+            onMouseOut={onMuseOverOut}>
                 <td>{item[0]}</td>
                 <td>{item[1]}</td>
-                <button onClick={onClickButtonOK}>{item[0]}</button>
+                <td><button onClick={(ev) => onClick(ev)}>{item[0]}</button></td>
             </tr>
+            
             )
+        
 })
     }
         </table>
